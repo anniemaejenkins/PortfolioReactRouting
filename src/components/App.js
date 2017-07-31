8416bfcd086ef13c2deaf337d6bba0dc625655b2
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-31 09:35:26 -0400

    made components

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../styles/App.css';
 
+// import Component
+import BaseLayout from './BaseLayout.js';
+
 class App extends Component {
   render() {
     return (
---
+++ src/components/BaseLayout.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+
+// import dumb components
+import Home from './Home.js';
+import About from './About.js';
+import Portfolio from './Portfolio.js';
